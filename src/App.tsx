1aaec8b58139f2bed7e3f2cdc12897daa58d43ac
bgrando24 <bgrando24@gmail.com>
2022-12-11 17:40:41 +1100

    Adding pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Home } from './pages/home/home';
 
 function App() {
   return (
     <div className="App">
       
-      <div className=' font-bold flex justify-center'>Test</div>
+      <Home />
 
     </div>
   );
