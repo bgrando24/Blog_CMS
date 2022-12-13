e42413a40c0cf7e886530c918a271a0f718a1f43
bgrando24 <bgrando24@gmail.com>
2022-12-13 17:18:17 +1100

    Added form template for new article

--- src/pages/publish/NewArticle.tsx
+++ src/pages/publish/NewArticle.tsx
@@ -1,8 +1,43 @@
 
 export const NewArticle = () => {
     return (
-        <div>
-            Publish a new article
+        <div className=" flex flex-col items-center py-10 text-xl max-w-[1080px] mx-auto">
+
+            <h1 className="font-bold mb-5">Publish a new article</h1>
+
+            <form className=" w-full flex flex-col items-center border-[1px] py-5 px-2">
+
+                <label htmlFor="title">Title</label>
+                <input 
+                    id="title" 
+                    name="title" 
+                    type="text" 
+                    className="w-[50%] mb-5 bg-gray-100 shadow rounded-sm" 
+                />
+
+                <label htmlFor="content">Content</label>
+                <textarea 
+                    id="content" 
+                    name="content"  
+                    className=" w-full h-32 mb-5 bg-gray-100 shadow rounded-sm text-sm" 
+                />
+
+                <label htmlFor="snippet">Snippet</label>
+                <input 
+                    id="snippet" 
+                    name="snippet" 
+                    type="text" 
+                    className="w-[50%] mb-5 bg-gray-100 shadow rounded-sm" 
+                />
+
+                <label htmlFor="author">Author</label>
+                <input 
+                    id="author" 
+                    name="author" 
+                    type="text" 
+                    className="w-[50%] mb-5 bg-gray-100 shadow rounded-sm" 
+                />
+            </form>
         </div>
     )
 }
