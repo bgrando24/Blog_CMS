c4972140e67944dde86137743e3d566e80c2698e
bgrando24 <bgrando24@gmail.com>
2022-12-13 15:29:35 +1100

    Added ability to consume node API on home page, currently only testing

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,10 +2,40 @@
 import { Articles } from "../../FakeDatabase";
 import { LatestArticles } from "./components/LatestArticles";
 import { TrendingArticles } from "./components/TrendingArticles";
+import { useEffect, useState } from 'react';
+import { ServerInfo } from "../../shared/ServerInfo";
+
 
 export function Home() {
+    const [serverStatus, setServerStatus] = useState("");
+
+
+    const CheckServerConnection = async () => {
+        try {
+
+            const response = await fetch(`${ServerInfo.DEV_DOMAIN}/home-test`, {
+                method: "GET",
+            });
+
+            const responseJSON = await response.json();
+            setServerStatus(responseJSON.message);
+            console.log("Server status retrieved");
+
+        } catch (e: any) {
+            console.error(e.message);
+        }
+    }
+
+    useEffect(() => {
+        CheckServerConnection();
+    }, []);
+
     return (
         <div className=" flex flex-col items-center w-full max-w-[1080px] mx-auto">
+
+            <p className=" my-5 p-1 border-red-600 border-[1px]">
+                Server status: {serverStatus ? serverStatus : "Not found"}
+            </p>
 
             <h1 className="text-bold text-xl py-10">
                 My Blog!
---
+++ src/shared/ServerInfo.js
@@ -0,0 +1,3 @@
+export const ServerInfo = {
+    DEV_DOMAIN: "http://localhost:5000"
+}
